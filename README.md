2d97d23ce2dafe822f53f67654b6b94b3b3e2487
Léo POIROUX <leo.poiroux@gmail.com>
2019-06-06 12:14:49 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# woody-acf-sync
+## ✨ Features
+
 A WP CLI command to synchronize ACF fields
+
+## 👉🏻 Installation & Usage
+
+> **Requires:**
+- **[PHP 5.6+](https://php.net/releases/)**
+
+First, install PHP Insights via the Composer package manager:
+```bash
+composer require raccourci/woody-acf-sync
+```
+
+You need to have ACF Pro and WP Cli installed. And the [ACF Pro synchronize feature](https://www.advancedcustomfields.com/resources/synchronized-json/synchronize) enabled.
+
+Synchronize the fields with the WP Cli command:
+```bash
+wp acf sync
+```
+
+## 👏 Contributors
+
+Thank you to all the people who have already contributed to Woody ACF Sync !
+Special thank to Jesse Pearson for his inspirational topic on this forum
+[Automatic Synchronized Json](https://support.advancedcustomfields.com/forums/topic/automatic-synchronized-json)
+
+## 🆓 License
+PHP Insights is open-sourced software licensed under the [GPL2](LICENSE).
---
+++ woody-acf-sync.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Plugin Name: Woody ACF Sync
+ * Plugin URI: https://github.com/raccourci/woody-acf-sync
+ * Version: 1.0
+ * Description: A WP CLI command to synchronize ACF fields
+ * Author: Raccourci Agency
+ * Author URI: https://www.raccourci.fr
+ * License: GPL2
+ *
+ * This program is GLP but; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of.
+ */
+
+class Woody_ACF_Sync_Command
+{
+    function sync($args, $assoc_args)
+    {
+        $groups = acf_get_field_groups();
+        $sync   = [];
+
+        if (empty($groups)) {
+            return WP_CLI::success("No ACF Sync required");
+        }
+
+        foreach ($groups as $group) {
+            $local    = acf_maybe_get($group, 'local', false);
+            $modified = acf_maybe_get($group, 'modified', 0);
+            $private  = acf_maybe_get($group, 'private', false);
+
+            if ($local !== 'json' || $private) {
+                // do nothing
+            } elseif (!$group['ID']) {
+                $sync[$group['key']] = $group;
+            } elseif ($modified && $modified > get_post_modified_time('U', true, $group['ID'], true)) {
+                $sync[$group['key']] = $group;
+            }
+        }
+
+        if (empty($sync)) {
+            return WP_CLI::success("No ACF Sync required");
+        }
+
+        if (!empty($sync)) {
+            foreach ($sync as $key => $v) {
+                if (acf_have_local_fields($key)) {
+                    $sync[$key]['fields'] = acf_get_local_fields($key);
+                }
+                acf_import_field_group($sync[$key]);
+            }
+            WP_CLI::success('ACF has been successfully synchronized');
+        }
+    }
+}
+
+if (defined('WP_CLI') && WP_CLI) {
+    WP_CLI::add_command('acf', 'Woody_ACF_Sync_Command');
+}
